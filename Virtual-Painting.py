8faf468b2d3cd0cff54e57f3b43250142c8fabe4
Yusuf Abdullatif <107510170+Igneyous@users.noreply.github.com>
2023-09-21 22:37:09 +0300

    added improvements to findColor function

--- Virtual-Painting.py
+++ Virtual-Painting.py
@@ -8,20 +8,32 @@ cap.set(3, frameWidth)
 cap.set(4, frameHeight)
 cap.set(10,150)
 
-myColors = [[5,93,0,179,255,255],
-            [0,96,113,179,255,255],
-            [0,88,139,179,255,255]]
+myColors = [[0,116,121,179,255,255],
+            [42,74,60,141,255,255],
+            [5,102,129,179,255,255]]
 
 def findColor(im, myColors):
     imgHSV = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
     for color in myColors:
         lower = np.array(color[0:3])
         upper = np.array(color[3:6])
-    mask = cv2.inRange(imgHSV, lower, upper)
-    cv2.imshow(str(color[0]), mask)
+        mask = cv2.inRange(imgHSV, lower, upper)
+        #cv2.imshow(str(color[0]), mask)
+
+def getContours(img):
+    contours, hierarchy = cv2.findContours(img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+    for cnt in contours:
+        area = cv2.contourArea(cnt)
+        if area>500:
+            cv2.drawContours(imgResult, cnt, -1, (255, 0, 0), 3)
+            peri = cv2.arcLength(cnt, True)
+            approx = cv2.approxPolyDP(cnt, 0.02*peri, True)
+            x,y,w,h = cv2.boundingRect(approx)
+
 
 while True:
     success, img = cap.read()
+    imgResult = img.copy()
     findColor(img, myColors)
     cv2.imshow('Result', img)
     if cv2.waitKey(1) & 0xFF == ord('q'):
