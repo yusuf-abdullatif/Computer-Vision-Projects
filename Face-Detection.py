05d6f67694b2a3a79d4b9e38d35f6ec1e908d0dd
Yusuf Abdullatif <107510170+Igneyous@users.noreply.github.com>
2023-11-07 21:00:13 +0300

    Fixed a mistake in loading the haarcascade file

--- Face-Detection.py
+++ Face-Detection.py
@@ -1,14 +1,44 @@
 import cv2
 
-faceCascade= cv2.CascadeClassifier("Resources/haarcascade_frontalface_default.xml")
-img = cv2.imread('Resources/lena.png')
-imgGray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+# Load the pre-trained Haar Cascade classifier for face detection
+face_cascade = cv2.CascadeClassifier('Resources/haarcascade_frontalface_default.xml')
 
-faces = faceCascade.detectMultiScale(imgGray,1.1,4)
+if face_cascade.empty():
+    print("Error: Could not load the Haar Cascade classifier.")
+    exit(1)
 
-for (x,y,w,h) in faces:
-    cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
+# Initialize the webcam
+cap = cv2.VideoCapture(0)
 
+if not cap.isOpened():
+    print("Error: Could not open the webcam.")
+    exit(1)
 
-cv2.imshow("Result", img)
-cv2.waitKey(0)
+while True:
+    # Read a frame from the webcam
+    ret, frame = cap.read()
+
+    if not ret:
+        print("Error: Could not read a frame from the webcam.")
+        break
+
+    # Convert the frame to grayscale for face detection
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    # Detect faces in the frame
+    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
+
+    # Draw rectangles around detected faces
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+    # Display the frame with detected faces
+    cv2.imshow('Live Face Detection', frame)
+
+    # Exit the program when the 'q' key is pressed
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# Release the video capture object and close the OpenCV window
+cap.release()
+cv2.destroyAllWindows()
